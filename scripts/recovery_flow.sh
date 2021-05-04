c219e9aaf760405f000e60546be097775b508d08
delanoNieh <delanonieh@gmail.com>
2021-05-04 21:02:58 +0800

    scripts update

---
+++ scripts/recovery_flow.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+root_path=$(cd $(dirname $0) && pwd)/..
+data_path=$root_path/data
+src_path=$root_path/src
+save_dir=$root_path/result/train/verX_occ25/flow
+origin_data="JHMDB_25"
+target_data="JHMDB"
+model_file=$save_dir/model_last.pth
+
+cd $data_path
+if [ ! -d "$origin_data" ]; then
+  echo "[ERROR] $origin_data does not exist"
+  exit 1
+fi
+if [ -d "$target_data" ]; then
+  echo "[ERROR] $target_data exists"
+  exit 1
+fi
+mv "$origin_data" "$target_data"
+
+cd $src_path
+
+python3 train.py --dataset hmdb --split 1 --lr 5e-4 --lr_step 12,20 --ninput 5 \
+--batch_size 8 --master_batch 8 --num_workers 4 --gpus 0 --num_epochs 30 \
+--auto_stop --flow_model $save_dir --load_model $model_file --start_epoch X
+
+cd $data_path
+mv "$target_data" "$origin_data"
+exit 0
